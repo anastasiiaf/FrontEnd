ce210bb67c02e52db643e56b3a6fcf99e7994131
Anastasiia <anastasiia_filchenko@yahoo.com>
2020-05-12 23:56:21 +0200

    array

---
+++ JavaScript/Basics/ToDoList/script.js
@@ -0,0 +1,20 @@
+/*var todos = ["Buy New Turtle"];
+window.setTimeout(function() {
+  // put all the rest of your JS code from the lecture here
+}, 500);*/
+
+window.setTimeout(function () {
+  var todos = ['dfdf', 'dfdf', 'sdgdfg'];
+  var input = prompt('What would you like to do?');
+  while (input !== 'quit') {
+    if (input === 'list') {
+      console.log(todos);
+    } else if (input === 'new') {
+      var newToDo = prompt('Enter new ToDo');
+      todos.push(newToDo);
+      input = prompt('What would you like to do?');
+    }
+  }
+
+  console.log('You quit the app!');
+}, 1000);
