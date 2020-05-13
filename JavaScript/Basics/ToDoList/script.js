990a147a403c414ee8d2ddaf2c1e4f8b80fe1bd5
Anastasiia <anastasiia_filchenko@yahoo.com>
2020-05-13 08:27:29 +0200

    forEach & splice

--- JavaScript/Basics/ToDoList/script.js
+++ JavaScript/Basics/ToDoList/script.js
@@ -1,20 +1,36 @@
-/*var todos = ["Buy New Turtle"];
-window.setTimeout(function() {
-  // put all the rest of your JS code from the lecture here
-}, 500);*/
-
+var todos = ['buy groceries', 'clean room', 'call home'];
 window.setTimeout(function () {
-  var todos = ['dfdf', 'dfdf', 'sdgdfg'];
-  var input = prompt('What would you like to do?');
-  while (input !== 'quit') {
-    if (input === 'list') {
-      console.log(todos);
-    } else if (input === 'new') {
-      var newToDo = prompt('Enter new ToDo');
-      todos.push(newToDo);
-      input = prompt('What would you like to do?');
+  var answer = prompt('What would you like to do?');
+  while (answer !== 'quit') {
+    if (answer === 'list') {
+      listToDos();
+    } else if (answer === 'new') {
+      addToDo();
+    } else if (answer === 'delete') {
+      deleteToDo();
     }
+    answer = prompt('What would you like to do?');
   }
 
   console.log('You quit the app!');
 }, 1000);
+
+function listToDos() {
+  console.log('**********');
+  todos.forEach(function (todo, i) {
+    console.log(i + ': ' + todo);
+  });
+  console.log('**********');
+}
+
+function addToDo() {
+  var newToDo = prompt('Enter new ToDo');
+  todos.push(newToDo);
+  console.log('Added ToDo');
+}
+
+function deleteToDo() {
+  var index = prompt('Enter index of ToDo to delete');
+  todos.splice(index, 1);
+  console.log('Deleted ToDo');
+}
