c2af893d5b5b4ec3d90fa4919853f6491ff54b11
Anastasiia <anastasiia_filchenko@yahoo.com>
2020-05-20 19:56:42 +0200

    drawing grid in js

--- Projects/Tetris/script.js
+++ Projects/Tetris/script.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid');
-  let squares = Array.from(document.querySelectorAll('.grid div'));
+  let gridContainer = document.getElementById('#grid');
+  //let squares = Array.from(document.querySelectorAll('.grid div'));
   const scoreDisplay = document.getElementById('#score');
   const startBtn = document.getElementById('#startBtn');
-  const width = 10;
-
+  //let width = 30;
+  let grid = [];
+  const width = createGrid();
   // Tetrominoes
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
@@ -16,13 +17,42 @@ document.addEventListener('DOMContentLoaded', () => {
   const theTetrominoes = [lTetromino];
 
   let currentPosition = 4;
-  let current = theTetrominoes[0][0];
+  let currentRotation = 0;
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+
+  let current = theTetrominoes[random][currentRotation];
 
   //draw
   function draw() {
     current.forEach((index) => {
-      squares[currentPosition + index].classList.add('tetromino');
+      console.log(width);
+      grid[currentPosition + index].classList.add('tetromino');
     });
   }
-  draw();
+
+  //undraw
+  function undraw() {
+    current.forEach((index) => {
+      console.log(width);
+      grid[currentPosition + index].classList.remove('tetromino');
+    });
+  }
+
+  //create grid
+  function createGrid() {
+    let positionInfo = gridContainer.getBoundingClientRect();
+    let gridHeight = positionInfo.height;
+    let gridWidth = positionInfo.width;
+
+    for (let i = 0; i < (gridHeight * gridWidth) / 400; i++) {
+      let square = document.createElement('div');
+      square.setAttribute('class', 'square');
+      gridContainer.appendChild(square);
+      grid.push(square);
+    }
+    let tableWidth = Math.floor(gridWidth / 20);
+    return tableWidth;
+  }
+
+  //draw();
 });
