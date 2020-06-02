81c3f35669e2d2aee341b84b1988f5e6057d8cd0
Anastasiia <anastasiia_filchenko@yahoo.com>
2020-06-02 22:15:38 +0200

    working with assets: css, ejs templates

--- JavaScript/NodeJS/EJSDemo/app.js
+++ JavaScript/NodeJS/EJSDemo/app.js
@@ -1,21 +1,21 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs'); // looks in folder views - no need to specify .ejs in res.render()
 
 app.get('/', function (req, res) {
   //res.send('<h1>Welcome to the home page!</h1>');
   // embeded javascript
-  res.render('home.ejs');
+  res.render('home');
 });
 
 app.get('/fallinlovewith/:thing', function (req, res) {
   var thing = req.params.thing;
-  res.render('love.ejs', { thingVar: thing });
+  res.render('love', { thingVar: thing });
 });
 
 app.get('/', function (req, res) {
-  //res.send('<h1>Welcome to the home page!</h1>');
-  // embeded javascript
-  res.render('home.ejs');
+  res.render('home');
 });
 
 app.get('/posts', function (req, res) {
