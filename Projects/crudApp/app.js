3ac2e57dfd5c5adc8b2f1488bde20fc043315f46
Anastasiia <anastasiia_filchenko@yahoo.com>
2020-06-05 23:57:56 +0200

    GET, POT, PUT, DELETE routs + static html

--- Projects/crudApp/app.js
+++ Projects/crudApp/app.js
@@ -3,9 +3,74 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('./db');
 
-const collection = 'todo';
+const collection = 'todos';
 const app = express();
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/getToDos', (req, res) => {
+  db.getDB()
+    .collection(collection)
+    .find({})
+    .toArray((err, documents) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(documents);
+        res.json(documents);
+      }
+    });
+});
+
+app.put('/:id', (req, res) => {
+  const todoID = req.params.id;
+  const userInput = req.body;
+
+  db.getDB()
+    .collection(collection)
+    .findOneAndUpdate(
+      { _id: db.getPrimaryKey(todoID) },
+      { $set: { todo: userInput.todo } },
+      { returnOriginal: false },
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.json(result);
+        }
+      },
+    );
+});
+
+app.post('/', (req, res) => {
+  const userInput = req.body;
+  db.getDB()
+    .collection(collection)
+    .insertOne(userInput, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json({ result: result, document: result.ops[0] });
+      }
+    });
+});
+
+app.delete('/:id', (req, res) => {
+  const todoID = req.params.id;
+
+  db.getDB()
+    .collection(collection)
+    .findOneAndDelete({ _id: db.getPrimaryKey(todoID) }, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        re.json(result);
+      }
+    });
+});
 
 db.connect((err) => {
   if (err) {
