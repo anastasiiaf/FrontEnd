14571cb99d7502a607c95f92faca5718c273c7b7
Anastasiia <anastasiia_filchenko@yahoo.com>
2020-06-06 17:23:32 +0200

    create, edit and delete (update& msg bug); npm joi

--- Projects/crudApp/app.js
+++ Projects/crudApp/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const joi = require('joi');
 const db = require('./db');
 
 const collection = 'todos';
 const app = express();
+const schema = joi.object().keys({
+  todo: joi.string().required(),
+});
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
@@ -47,15 +51,31 @@ app.put('/:id', (req, res) => {
 
 app.post('/', (req, res) => {
   const userInput = req.body;
-  db.getDB()
-    .collection(collection)
-    .insertOne(userInput, (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json({ result: result, document: result.ops[0] });
-      }
-    });
+
+  joi.validate(userInput, schema, (err, result) => {
+    if (err) {
+      const error = new Error('Invalid Input');
+      error.status = 400;
+      next(error);
+    } else {
+      db.getDB()
+        .collection(collection)
+        .insertOne(userInput, (err, result) => {
+          if (err) {
+            const error = new Error('Failed to insert ToDo');
+            error.status = 400;
+            next(error);
+          } else {
+            res.json({
+              result: result,
+              document: result.ops[0],
+              msg: 'Inserted ToDo',
+              error: null,
+            });
+          }
+        });
+    }
+  });
 });
 
 app.delete('/:id', (req, res) => {
@@ -67,9 +87,15 @@ app.delete('/:id', (req, res) => {
       if (err) {
         console.log(err);
       } else {
-        re.json(result);
+        res.json(result);
       }
     });
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status).json({
+    error: { message: err.message },
+  });
 });
 
 db.connect((err) => {
