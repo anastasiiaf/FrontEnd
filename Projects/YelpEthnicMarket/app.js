237dd8c47ed49b0a75cc4c96b04d1f50be775777
Anastasiia <anastasiia_filchenko@yahoo.com>
2020-06-07 23:58:37 +0200

    data persistence w/mongoose; added /:id page&desc

--- Projects/YelpEthnicMarket/app.js
+++ Projects/YelpEthnicMarket/app.js
@@ -1,22 +1,23 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 
-var market = [
-  {
-    name: 'Grønland Torg',
-    image: 'https://pbs.twimg.com/media/A8YxpuiCAAACxgY.jpg',
-  },
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(
+  'mongodb+srv://Anastasiia:250591Rada@cluster0-v3ypj.mongodb.net/YelpEthnicMarket?retryWrites=true&w=majority',
+);
 
-  {
-    name: 'Brugata Torg',
-    image: 'https://mappno.com/yer/gr-nlandtorg-frukt-gr-nt-3225.jpg',
-  },
-  {
-    name: 'Lambertseter Torg',
-    image: 'https://s3-media0.fl.yelpcdn.com/bphoto/LpU0vCDv6fqLSNRF45d64Q/300s.jpg',
-  },
-];
+// Schema setup
+
+var shopSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
+});
+
+var ethnicMarket = mongoose.model('market', shopSchema);
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,20 +26,48 @@ app.get('/', function (req, res) {
   res.render('landing');
 });
 
+// INDEX - show all shops in the market
 app.get('/market', function (req, res) {
-  res.render('market', { market: market });
+  ethnicMarket.find({}, function (err, shops) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', { market: shops });
+    }
+  });
 });
 
+// CREATE - add new shop to the market
 app.post('/market', function (req, res) {
   var name = req.body.name;
   var image = req.body.image;
-  var newShop = { name: name, image: image };
-  market.push(newShop);
-  res.redirect('/market'); // default is to redirect as get route
+  var description = req.body.description;
+  var newShop = { name: name, image: image, description: description };
+  ethnicMarket.create(newShop, function (err, shop) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/market'); // default is to redirect as GET route
+    }
+  });
 });
 
+// NEW - show form to create a new shop
 app.get('/market/new', function (req, res) {
   res.render('new');
+});
+
+//SHOW - shows more info about shop
+// '/market/:id' - this rout should be after '/market/new' GET rout
+// otherwise, it will be overwritten
+app.get('/market/:id', function (req, res) {
+  ethnicMarket.findById(req.params.id, function (err, foundShop) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('show', { market: foundShop });
+    }
+  });
 });
 
 app.listen(3000, process.env.IP, function () {
