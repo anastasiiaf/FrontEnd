1ed91a29606f3651042aabf06d8aa8b6092aefaf
Anastasiia <anastasiia_filchenko@yahoo.com>
2020-06-13 10:08:32 +0200

    embedded data

--- JavaScript/NodeJS/Associations/embed.js
+++ JavaScript/NodeJS/Associations/embed.js
@@ -5,22 +5,27 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.connect(
   'mongodb+srv://Anastasiia:250591Rada@cluster0-v3ypj.mongodb.net/data?retryWrites=true&w=majority',
 );
-
-var userSchema = new mongoose.Schema({
-  email: String,
-  name: String,
-});
-var User = mongoose.model('User', userSchema);
-
 var postSchema = new mongoose.Schema({
   title: String,
   content: String,
 });
-var postModel = mongoose.model('Post', postSchema);
+var Post = mongoose.model('Post', postSchema);
+
+var userSchema = new mongoose.Schema({
+  email: String,
+  name: String,
+  posts: [postSchema],
+});
+var User = mongoose.model('User', userSchema);
 
 var newUser = new User({
-  email: 'joan@yahoo.com',
-  name: 'Joan Willis',
+  email: 'henry@yahoo.com',
+  name: 'Henry Willis',
+});
+
+newUser.posts.push({
+  title: 'Groceries at Brugata',
+  content: 'did not find mushrooms...',
 });
 
 newUser.save(function (err, user) {
@@ -28,5 +33,38 @@ newUser.save(function (err, user) {
     console.log(err);
   } else {
     console.log(user);
+  }
+});
+
+/* var newPost = new Post({
+  title: 'Apples in Lamber market',
+  content: 'the are delicious',
+});
+
+newPost.save(function (err, user) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(user);
+  }
+}); */
+
+User.findOne({ name: 'Henry Willis' }, function (err, user) {
+  if (err) {
+    console.log(err);
+  } else {
+    // push new post
+    user.posts.push({
+      title: 'Meat at Grønland',
+      content: 'big variety, low prices',
+    });
+    // save to db; user in callback will have a new post
+    user.save(function (err, user) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(user);
+      }
+    });
   }
 });
