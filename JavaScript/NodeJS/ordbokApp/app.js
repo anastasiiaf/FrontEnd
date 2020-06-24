4a2d9d83ddac22581bf65b207d931a641ac7561b
Anastasiia <anastasiia_filchenko@yahoo.com>
2020-06-25 00:06:36 +0200

    .

--- JavaScript/NodeJS/ordbokApp/app.js
+++ JavaScript/NodeJS/ordbokApp/app.js
@@ -1,16 +1,17 @@
 var ordbok = require('ordbok'),
-  express = require('express'),
-  request = require('request');
+  express = require('express');
+
 var app = express();
 app.set('view engine', 'ejs');
-var options = { word: 'lese' };
+const fetch = require('node-fetch');
+//var options = { word: 'lese' };
+var options = { word: 'lesende' };
 
 //var striptags = require('striptags');
 const stripHtml = require('string-strip-html');
 const { extract } = require('article-parser');
 
 //const url = 'https://www.klartale.no/norge/mener-folk-betaler-for-mye-for-mat-1.1733276';
-
 /* extract(url)
   .then((article) => {
     console.log(stripHtml(article.content));
@@ -22,7 +23,43 @@ const { extract } = require('article-parser');
 
 ordbok(options, function (error, data) {
   //console.log(data);
+  //console.log(error);
   //console.log(data['bokmal'][0]['paradigm']);
   //console.log(data['bokmal'][0]['interpretation']);
   //console.log(data['bokmal'][0]['interpretation'][0]['definition']);
+});
+
+const request = require('request');
+const uri = 'http://ordbok.uib.no/perl/ordbok.cgi';
+const parser = require('parser');
+let reqOpts = {
+  begge: '+&ordbok=begge',
+};
+
+function OB(opts, callback) {
+  if (!opts.word) {
+    return callback(new Error('Missing required param: word'), null);
+  }
+
+  reqOpts.OPP = opts.word;
+
+  request(uri, { qs: reqOpts }, function (error, response, body) {
+    if (error) {
+      return callback(error, null);
+    }
+
+    parser(body.toString(), function (err, data) {
+      if (err) {
+        return callback(err, null);
+      }
+      return callback(null, data);
+    });
+  });
+}
+
+OB({ word: query }, function (err, data) {
+  if (err) {
+    throw err;
+  }
+  console.log(JSON.stringify(data));
 });
