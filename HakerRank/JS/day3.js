7f4d9621bafeb7dfe742926d0a0fd077e578b594
Anastasiia <anastasiia_filchenko@yahoo.com>
2020-08-16 22:51:03 +0200

    using map

--- HakerRank/JS/day3.js
+++ HakerRank/JS/day3.js
@@ -4,8 +4,24 @@ function vowelsAndConsonants(s) {
   let consonantRegex = new RegExp(`(?![${vowels}])[a-z]`, 'gi');
   let word = s.split('');
 
-  let cons = [];
+  console.log('using map');
+  // print vowels
+  word.map(function (el) {
+    if (vowels.includes(el)) {
+      console.log(el);
+    }
+  });
 
+  // print conosants
+  word.map(function (el) {
+    if (vowels.includes(el) === false) {
+      console.log(el);
+    }
+  });
+
+  // using loops
+  console.log('using loops...');
+  let cons = [];
   for (let i = 0; i < word.length; i++) {
     if (vowels.includes(word[i])) {
       console.log(word[i]);
