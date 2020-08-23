2c456ce5cb878c733d64eefbb9893b1ecf9971df
Anastasiia <anastasiia_filchenko@yahoo.com>
2020-08-23 21:33:37 +0200

    WIP

---
+++ JavaScript/DailyCodingProblem/medium/shortestSubstring.js
@@ -0,0 +1,36 @@
+/*
+Given a string and a set of characters,
+return the shortest substring containing all the characters in the set.
+*/
+
+function shortestSubstring(word, substr) {
+  let substringIndeces = [];
+
+  for (let i = 0; i < substr.length; i++) {
+    let arr = [];
+    let size = 0;
+    let isInWord = true;
+    let prevPosition = 0;
+    while (isInWord) {
+      let ind = word.indexOf(substr[i], prevPosition);
+      console.log(i);
+      if (ind > -1) {
+        arr.push(ind);
+        prevPosition = ind + 1;
+      } else {
+        isInWord = false;
+      }
+    }
+
+    if (arr.length > 0) {
+      substringIndeces.push(arr);
+      if (arr.length > size) {
+        size = arr.length;
+      }
+    }
+  }
+
+  console.log(substringIndeces);
+}
+
+console.log(shortestSubstring('figehaeci', ['a', 'e', 'i']));
